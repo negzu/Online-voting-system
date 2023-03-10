4fecfc0fe01b1d301bf1a2253bfb50bf2f738ce4
negzu <naron122@gmail.com>
2023-03-10 12:10:02 +0800

    Online voting System
    Online voting system That has Facial recognition and biometrics using Fire base as data base

---
+++ Online Voting System/app/src/main/java/com/onlie/voting/onlinevotingsystem/MainActivity.java
@@ -0,0 +1,30 @@
+package com.onlie.voting.onlinevotingsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView welcome,welcome2,welcome3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+
+
+            }
+        },3000);
+
+    }
+}
